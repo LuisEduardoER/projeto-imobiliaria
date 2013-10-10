ed917c708e7a9c653902f01c6ff84e7d81590aad
brunocccc@gmail.com <brunocccc@gmail.com@8bcb777c-db69-b16f-aee1-2c4e2ccdad5a>
2013-10-10 16:04:32 +0000

    implementação do cadastro de usuarios/funcionarios

---
+++ TCC/src/Componentes/TableModelFuncionario.java
@@ -0,0 +1,139 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Componentes;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+import modelo.Funcionario;
+
+/**
+ *
+ * @author Bruno
+ */
+public class TableModelFuncionario extends AbstractTableModel {
+
+    // Lista de Sócios a serem exibidos na tabela
+    private List<Funcionario> linhas;
+
+    
+    
+    // Cria um SocioTableModel sem nenhuma linha
+    public TableModelFuncionario() {
+        linhas = new ArrayList<Funcionario>();
+    }
+
+    // Cria um SocioTableModel contendo a lista recebida por parâmetro
+    public TableModelFuncionario(List<Funcionario> listaDeFuncionario) {
+        linhas = new ArrayList<Funcionario>(listaDeFuncionario);
+    }
+    private String[] colunas = new String[]{"Codigo", "Nome", "CPF", "Login"};
+    private static final int CODIGO = 0;
+    private static final int NOME = 1;
+    private static final int CPF = 2;
+    private static final int LOGIN = 3;
+
+    @Override
+    public int getRowCount() {
+        return linhas.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return colunas.length;
+    }
+
+    @Override
+    public String getColumnName(int columnIndex) {
+        return colunas[columnIndex];
+    }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        switch (columnIndex) {
+            case CODIGO:
+                return Integer.class;
+            case NOME:
+                return Integer.class;
+            case CPF:
+                return String.class;
+            case LOGIN:
+                return Float.class;
+            default:
+                throw new IndexOutOfBoundsException("columnIndex out of bounds");
+        }
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        return false;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        // Pega o sócio referente a linha especificada.
+        Funcionario funcionario = linhas.get(rowIndex);
+
+        switch (columnIndex) {
+            case CODIGO:
+                return funcionario.getProduto_id();
+            case NOME:
+                return funcionario.getProdutoCodigoBarras();
+            case CPF:
+                return funcionario.getProdutoNome();
+            case LOGIN:
+                return funcionario.getValor();
+            default:
+                throw new IndexOutOfBoundsException("columnIndex out of bounds");
+        }
+    }
+
+    @Override
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+        // Pega o sócio referente a linha especificada.
+        Funcionario funcionario = linhas.get(rowIndex);
+
+        switch (columnIndex) {
+
+            case CODIGO_BARRAS:
+                funcionario.setProdutoCodigoBarras((String) aValue);
+                break;
+            case PRODUTO:
+                funcionario.setProdutoNome((String) aValue);
+            case VALOR:
+                funcionario.setValor((Float) aValue);
+                break;
+            default:
+                throw new IndexOutOfBoundsException("columnIndex out of bounds");
+        }
+        fireTableCellUpdated(rowIndex, columnIndex);
+    }
+
+    public Funcionario getFuncionario(int indiceLinha) {
+        return linhas.get(indiceLinha);
+    }
+
+    public void addFuncionario(Funcionario funcionarios) {
+        linhas.add(funcionarios);
+        int ultimoIndice = getRowCount() - 1;
+        fireTableRowsInserted(ultimoIndice, ultimoIndice);
+    }
+
+    public void removeProduto(int indiceLinha) {
+        linhas.remove(indiceLinha);
+        fireTableRowsDeleted(indiceLinha, indiceLinha);
+    }
+
+    public void addListaDeProdutos(List<Funcionario> funcionarios) {
+        int indice = getRowCount();
+        linhas.addAll(funcionarios);
+        fireTableRowsInserted(indice, indice + funcionarios.size());
+    }
+
+    public void limpar() {
+        linhas.clear();
+        fireTableDataChanged();
+    }
+}
--- TCC/src/modelo/Endereco.java
+++ TCC/src/modelo/Endereco.java
@@ -9,6 +9,8 @@ import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -30,6 +32,7 @@ public class Endereco implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
     @Column(name = "enderecoID")
     private Integer enderecoID;
