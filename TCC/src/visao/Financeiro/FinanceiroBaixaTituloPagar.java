5805a1c21bc1607daa49cd7662c11eaea1482194
brunocccc@gmail.com <brunocccc@gmail.com@8bcb777c-db69-b16f-aee1-2c4e2ccdad5a>
2013-10-14 12:29:23 +0000

    organização das pastas / Abertura Fechamento de Caixa

--- TCC/src/visao/Financeiro/FinanceiroBaixaTituloPagar.java
+++ TCC/src/visao/Financeiro/FinanceiroBaixaTituloPagar.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package visao;
+package visao.Financeiro;
 
 import Componentes.Componentes;
 import controller.Cadastro.FornecedorController;
--- TCC/src/visao/Venda/VendaTela.java
+++ TCC/src/visao/Venda/VendaTela.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package visao;
+package visao.Venda;
 
 import controller.Mensagens;
 import controller.Cadastro.ProdutoController;
